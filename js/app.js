f0b51ce858be015d4be03a9c2075aeb027485783
sapanab <basutkar.sapana3@gmail.com>
2015-04-18 10:42:35 +0530

    some changes

--- js/app.js
+++ js/app.js
@@ -118,5 +118,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'ConnectCtrl'
     });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/home');
+    $urlRouterProvider.otherwise('/landingpage');
 });
