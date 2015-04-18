f086f0f5ab2410b4c90e95f200ecec3c8d303582
sapanab <basutkar.sapana3@gmail.com>
2015-04-18 12:55:40 +0530

    changes for button

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
-            StatusBar.overlaysWebView(true);
             StatusBar.styleLightContent();
         }
     });
