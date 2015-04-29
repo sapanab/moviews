a0cc76b7cbe8309aee55ebfe61da4a72096b5bfe
Tushar <tushar@wohlig.com>
2015-04-29 18:05:16 +0530

    Fixed ionic.css

--- js/app.js
+++ js/app.js
@@ -33,16 +33,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'jagruticontroller'])
         controller: 'AppCtrl'
     })
 
-   
-    .state('app.browse', {
-        url: "/browse",
-        views: {
-            'menuContent': {
-                templateUrl: "templates/browse.html"
-            }
-        }
-    })
-
     .state('app.details', {
         url: "/details/:id",
         views: {
